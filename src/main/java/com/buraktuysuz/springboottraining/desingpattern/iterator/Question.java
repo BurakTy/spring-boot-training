d3eb96970bb7411d36386418ba1654ba89df0f70
BurakTy <tuysuzburak@gmail.com>
2021-12-26 18:27:33 +0300

    iterator design pattern

---
+++ src/main/java/com/buraktuysuz/springboottraining/desingpattern/iterator/Question.java
@@ -0,0 +1,14 @@
+package com.buraktuysuz.springboottraining.desingpattern.iterator;
+
+public class Question {
+
+    private Integer id;
+
+    public Question(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+}
